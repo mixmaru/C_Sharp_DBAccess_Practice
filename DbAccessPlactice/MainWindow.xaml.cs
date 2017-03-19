0a8ef8cec07bd94ad3bf96f83e64a2c84ea34f21
mix <mix.touch777@gmail.com>
2017-03-19 17:04:01 +0900

    社員データを表示

--- DbAccessPlactice/MainWindow.xaml.cs
+++ DbAccessPlactice/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using DbAccessPlactice.Common;
+using DbAccessPlactice.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +27,44 @@ namespace DbAccessPlactice
         {
             InitializeComponent();
         }
+    }
+
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public ObservableCollection<社員ViewModel> 社員viewModels { get; set; }
+
+        private 社員Model 社員model = new 社員Model();
+
+        public MainWindowViewModel()
+        {
+            this.社員viewModels = this.Crate社員ViewModelsBy部門番号(10);
+        }
+
+        private ObservableCollection<社員ViewModel> Crate社員ViewModelsBy部門番号(int 部門番号)
+        {
+            var retList = new ObservableCollection<社員ViewModel>();
+            foreach (var data in this.社員model.get社員ListBy部門番号(部門番号))
+            {
+                var obj = new 社員ViewModel()
+                {
+                    入社日 = data.入社日,
+                    氏名 = data.氏名,
+                    社員番号 = data.社員番号,
+                    給与 = data.給与,
+                    部門番号 = data.部門番号,
+                };
+                retList.Add(obj);
+            }
+            return retList;
+        }
+    }
+
+    public class 社員ViewModel : ViewModelBase
+    {
+        public int 社員番号 { get; set; }
+        public string 氏名 { get; set; }
+        public decimal? 給与 { get; set; }
+        public DateTime? 入社日 { get; set; }
+        public int 部門番号 { get; set; }
     }
 }
---
+++ DbAccessPlactice/Model/社員Model.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DbAccessPlactice.DB;
+using DbAccessPlactice.DB.社員DataSetTableAdapters;
+
+namespace DbAccessPlactice.Model
+{
+    class 社員Model
+    {
+        private 社員DataSet 社員dataSet = new 社員DataSet();
+        private 社員TableAdapter 社員tableAdapter = new 社員TableAdapter();
+
+        public 社員Model()
+        {
+        }
+
+        public List<社員取得用entity> get社員ListBy部門番号(int 部門番号)
+        {
+            var retList = new List<社員取得用entity>();
+            this.社員tableAdapter.Fill(this.社員dataSet.社員, 部門番号);
+            foreach (var record in this.社員dataSet.社員)
+            {
+                var obj = new 社員取得用entity()
+                {
+                    社員番号 = record.社員番号,
+                    氏名 = record.氏名,
+                    給与 = record.Is給与Null() ? null : (decimal?)record.給与,
+                    入社日 = record.Is入社日Null() ? null : (DateTime?)record.入社日,
+                    部門番号 = record.部門番号,
+                };
+                retList.Add(obj);
+            }
+            return retList;
+        }
+
+    }
+
+    class 社員取得用entity
+    {
+        public int 社員番号 { get; set; }
+        public string 氏名 { get; set; }
+        public decimal? 給与 { get; set; }
+        public DateTime? 入社日 { get; set; }
+        public int 部門番号 { get; set; }
+    }
+}
