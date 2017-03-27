1034130c10c5acf5eb197af5420a137774619a56
mix <mix.touch777@gmail.com>
2017-03-27 20:10:49 +0900

    いろいろ練習、実験

---
+++ バリデーション練習/ViewModel/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace バリデーション練習.ViewModel
+{
+    class Person : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _Name;
+        public string Name 
+        {
+            get
+            {
+                return this._Name;
+            }
+            set
+            {
+                this._Name = value;
+                if (this.PropertyChanged != null)
+                {
+                    this.PropertyChanged(this, new PropertyChangedEventArgs("Name"));
+                }
+            }
+        }
+
+        private int _Age;
+        public int Age
+        {
+            get
+            {
+                return this._Age;
+            }
+            set
+            {
+                this._Age = value;
+                if (this.PropertyChanged != null)
+                {
+                    this.PropertyChanged(this, new PropertyChangedEventArgs("Age"));
+                }
+            }
+        }
+    }
+}
---
+++ バリデーション練習/ViewModel/ViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace バリデーション練習.ViewModel
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public ViewModel()
+        {
+            this.PersonList = new List<Person>();
+
+            this.Load();
+        }
+
+        public List<Person> PersonList { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void Load()
+        {
+            this.PersonList.Add(new Person()
+            {
+                Name = "桜井 章",
+                Age = 20,
+            });
+            this.PersonList.Add(new Person()
+            {
+                Name = "草薙剛",
+                Age = 40,
+            });
+        }
+    }
+}
