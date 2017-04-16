c448b93df6c47fccd9c635243e4b5bd65d376a49
mix <mix.touch777@gmail.com>
2017-04-16 16:46:13 +0900

    数値以外が入力された場合に入力値を削除する方法の実験。Converterを使ったパターン

---
+++ ユーザーコントロール練習/Converter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ユーザーコントロール練習 {
+    class Converter : IValueConverter {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            int res;
+            if (int.TryParse((string)value, out res)) {
+                return res;
+            }else {
+                return null;
+            }
+        }
+    }
+}
---
+++ ユーザーコントロール練習/数値のみ許可TextBlock.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ユーザーコントロール練習 {
+    /// <summary>
+    /// 数値のみ許可TextBlock.xaml の相互作用ロジック
+    /// </summary>
+    public partial class 数値のみ許可TextBlock : UserControl, INotifyPropertyChanged {
+        public 数値のみ許可TextBlock() {
+            InitializeComponent();
+            this.TextBlock.Text = Value.ToString();
+        }
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(int), typeof(数値のみ許可TextBlock), new PropertyMetadata(0));
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Value {
+            get {
+                return (int)GetValue(ValueProperty);
+            }
+            set {
+                //数値でなければ無視する?
+
+
+                SetValue(ValueProperty, value);
+                if(this.PropertyChanged != null) {
+                    PropertyChanged(this, new PropertyChangedEventArgs("Value"));
+                }
+            }
+        }
+
+        private void TextBlock_LostFocus(object sender, RoutedEventArgs e) {
+            int res;
+            if (int.TryParse(this.TextBlock.Text, out res)) {
+                Value = int.Parse(this.TextBlock.Text);
+            }else {
+                this.TextBlock.Text = Value.ToString();
+            }
+        }
+    }
+}
